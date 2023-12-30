c8f9dd32e8e1bd69fb5a403b803adbb4cbded232
Oleksandr <sidrsidr2@gmail.com>
2023-12-30 12:46:46 +0200

    improve loading pictures

--- script.js
+++ script.js
@@ -4,18 +4,23 @@ const loader = document.getElementById('loader')
 let ready = false;
 let imagesLoaded = 0;
 let totalLoaded = 0;
+let isInitialLoad = true ;
 let photosArray = [];
 
-const count = 30;
+
+const count = 5;
 const apiKey = 'T1IXWgdr2-TRKWluB9DBTyH5qLoyF4ftXiQbgXFthCE';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+function updateAPIURLWithNewCount (picCount) {
+    apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${picCount}`;
+  }
 
 function imageLoaded(){
     imageLoaded++;
     if(imagesLoaded === totalLoaded){
         ready = true;
         loader.hidden = true;
-        console.log('ready=', ready)
     }
     
 }
@@ -52,13 +57,17 @@ function displayPhotos(){
 
 // Get Photos from API
 async function getPhotos (){
-    try{
+    try {
         const response = await fetch(apiUrl);
         photosArray = await response.json();
-        displayPhotos()
-    }catch (error) {
-
-    }
+        displayPhotos();
+        if (isInitialLoad) { 
+          updateAPIURLWithNewCount(30) 
+          isInitialLoad = false 
+        } 
+      } catch (error) {
+        // Catch Error Here
+      }
 }
 
 window.addEventListener('scroll', ()=>{
